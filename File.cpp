e4506bb1821b0ab2770a019a651d3a12bbccd50d
Dheeraj Kumar <cyberphoenix@outlook.in>
2022-04-01 22:41:31 +0530

    added save and saveAs feature

--- File.cpp
+++ File.cpp
@@ -7,20 +7,60 @@ File::File()
 
 void File::NewFile(QWidget* parent)
 {
+    IsSaved = false;
+    QString fileName = "Untitled.json";
+    //this part needs to be coded next day. Good Night
+}
+void File::Save(QWidget* parent)
+{
+    if(!IsSaved)
+    {
+        SaveAs(parent);
+    }else
+    {
+        saveFile(parent, file.fileName());
+    }
+}
+void File::SaveAs(QWidget* parent)
+{
+    QString fileName = QFileDialog::getSaveFileName(parent);
+    if (fileName.isEmpty())
+    {
+        return;
+    }
+    saveFile(parent, fileName);
+}
+void File::saveFile(QWidget* parent, const QString &fileName)
+{
+    QFile file(fileName);
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox msg(parent);
+        msg.setWindowTitle("Error");
+        msg.setText("Cannot Save file "+file.fileName());
+        msg.setIcon(QMessageBox::Warning);
+        msg.exec();
+        return;
+    }
 
+    QTextStream out(&file);
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+    out << jsonData;
+    QApplication::restoreOverrideCursor();
+    file.setFileName(fileName);
 }
 void File::LoadFile(QWidget* parent)
 {
     QString fileName = QFileDialog::getOpenFileName(parent, "Select json file.", "/", "Json files (*.json)");
     if (!fileName.isEmpty())
     {
+        IsSaved = true;
         file.setFileName(fileName);
         fileInfo.setFile(file);
         parent->setWindowTitle("JSON Explorer - " + fileInfo.fileName());
         if(!file.open(QFile::ReadOnly | QFile::Text))
         {
             QMessageBox msg(parent);
-            msg.setWindowTitle("File Read Error");
+            msg.setWindowTitle("Error");
             msg.setText("Cannot read file "+file.fileName());
             msg.setIcon(QMessageBox::Warning);
             msg.exec();
--- File.h
+++ File.h
@@ -14,10 +14,14 @@ public:
     QFile file;
     QFileInfo fileInfo;
     QString jsonData;
+    bool IsSaved = false;
     //Functions
     File();
     void LoadFile(QWidget* parent);
     void NewFile(QWidget* parent);
+    void Save(QWidget* parent);
+    void SaveAs(QWidget* parent);
+    void saveFile(QWidget* parent,const QString &fileName);
     void setJsonData(QString data);
     QString* getJsonData();
 };
